eec34e1ea8a2d0e81c8cc3f22b757b1dbc656c38
Marco Rojo <mrmarcondes@gmail.com>
2012-04-06 20:17:46 -0300

    Initial Commit

---
+++ FirstProjectTweeter/MGNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MGNAppDelegate.h
+//  FirstProjectTweeter
+//
+//  Created by Marco Antonio Rojo Marcondes on 06/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FirstProjectTweeter/MGNViewController.h
@@ -0,0 +1,13 @@
+//
+//  MGNViewController.h
+//  FirstProjectTweeter
+//
+//  Created by Marco Antonio Rojo Marcondes on 06/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGNViewController : UIViewController
+
+@end
